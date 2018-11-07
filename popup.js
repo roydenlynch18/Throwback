3de9ca573ec98f5b30db5995a453e94eb1380053
roydenlynch18 <roydenlynch18@gmail.com>
2018-11-07 15:42:23 -0600

    v0.3 - Multiple Sessions Can Be Successfully Saved and Retrieved from Google Storage

--- popup.js
+++ popup.js
@@ -1,5 +1,52 @@
 // popup.js
 
+// Define Function loadStoredSessions()
+// loadStoredSessions() querys all the current keys in google chrome's synced
+// storage to find and keys that start with "sessionUrls" because these are
+// the keys that store the applications saved sessions.
+function loadStoredSessions() {
+
+  // Retrieving Session Urls Keys from Storage
+
+  // Initialize an Empty Array Variable for the "sessionUrls" Items
+  var sessionUrlsKeys = []; //  Variable Must Be Defined Outside Chrome Call
+  // Grabbing Storeage
+  chrome.storage.sync.get(null, function(items) {
+    // Allocate Keys to an Array Variable
+    var allKeys = Object.keys(items);
+    // Counter Variable for Inner Array Defining
+    var j = 0;
+    // Query the Keys
+    for (var i = 0; i < allKeys.length; i++) {
+      // Check if Current Key is a "sessionUrls" Key
+      if (allKeys[i].substr(0,11) == "sessionUrls") {
+        // Save it to New Array
+        sessionUrlsKeys[j] = allKeys[i];
+        // Increment New Array's Counter
+        j++;
+      }
+    }
+    // Console Logging for Debugging.
+    chrome.extension.getBackgroundPage().console.log("All Keys Found in Storage: ");
+    chrome.extension.getBackgroundPage().console.log(allKeys);
+    chrome.extension.getBackgroundPage().console.log("Session Urls Keys Found in Storage: ");
+    chrome.extension.getBackgroundPage().console.log(sessionUrlsKeys);
+
+    // Displays the Rows for the Proper Session Urls Keys on the Popup
+
+    // Iterate Through Session Urls Keys
+    for (var i = 0; i < sessionUrlsKeys.length; i++) {
+      // Create the Row on the Popup
+      createRow(parseInt(sessionUrlsKeys[i].substr(sessionUrlsKeys[i].length-1), 10));
+      // Console Logging for Debugging.
+      chrome.extension.getBackgroundPage().console.log("Row "+parseInt(sessionUrlsKeys[i].substr(sessionUrlsKeys[i].length-1), 10)+" retrieved and displayed.");
+    }
+  });
+}
+// Calling loadStoredSessions() Function
+loadStoredSessions();
+
+
 let createNewSession = document.getElementById('createNewSession');
 
 createNewSession.onclick = function(element) {
@@ -9,13 +56,35 @@ createNewSession.onclick = function(element) {
   for (var i=0; i<elements.length; i++) {
     ids += elements[i].id;
   }
-  chrome.extension.getBackgroundPage().console.log("Ids:");
-  chrome.extension.getBackgroundPage().console.log(ids);
+  //chrome.extension.getBackgroundPage().console.log("Ids:");
+  //chrome.extension.getBackgroundPage().console.log(ids);
   var row_num = elements.length;
   createRow(row_num);
 
+
+
   //document.getElementById(elements[elements.length-1]).appendChild(div);
 }
+
+let wipeAllSessions = document.getElementById('wipeAllSessions');
+
+wipeAllSessions.onclick = function(element) {
+  chrome.storage.sync.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    var sessionUrlsKeys = [];
+    for (var i = 0; i < allKeys.length; i++) {
+      if (allKeys[i].substr(0,11) == "sessionUrls") {
+        chrome.storage.sync.remove(allKeys[i]);
+      }
+    }
+    chrome.extension.getBackgroundPage().console.log(allKeys);
+    chrome.extension.getBackgroundPage().console.log(sessionUrlsKeys);
+  });
+}
+
+// Defining References to Buttons on Dropdown Menu
+let saveSessionBtns = document.getElementsByClassName('saveSession');
+let openSessionBtns = document.getElementsByClassName('openSession');
 
 // Function to Create a New Row
 function createRow(row_num) {
@@ -36,8 +105,10 @@ function createRow(row_num) {
   // Creating New Buttons
   var saveSessionBtn = document.createElement("button");
   saveSessionBtn.id = "saveSession"+row_num.toString();
+  saveSessionBtn.setAttribute('class','saveSession');
   var openSessionBtn = document.createElement("button");
   openSessionBtn.id = "openSession"+row_num.toString();
+  openSessionBtn.setAttribute('class','openSession');
 
   paragraph.appendChild(text);
   divColText.appendChild(paragraph);
@@ -47,140 +118,91 @@ function createRow(row_num) {
   divRow.appendChild(divColBtns);
 
   document.body.appendChild(divRow);
-  //document.getElementById("row"+(row_num-1).toString()).appendChild(divRow);
 
-}
-
-// Defining References to Buttons on Dropdown Menu
-let saveSession1 = document.getElementById('saveSession1');
-let openSession1 = document.getElementById('openSession1');
-
-// Defining Function for Clicking Save Button
-saveSession1.onclick = function(element) {
-
-  // Defining Variable for Query Information on Tab Query
-  var queryInfo = {
-    currentWindow: true
-  }
-
-  // Querying Tabs
-  chrome.tabs.query(queryInfo, function(tabs) {
-
-    // Defining Variable to Store Session Urls
-    var sessionUrls1 = [];
-
-    // Looping Through Tabs
-    for (var i = 0; i < tabs.length; i++) {
-
-      // Storing the Tab Url in the Session Array
-      sessionUrls1[i] = tabs[i].url;
-
-    };
-
-    //chrome.extension.getBackgroundPage().console.log(sessionUrls);
-
-    // Saving the Session Urls in Storage
-    chrome.storage.sync.set({'sessionUrls1': sessionUrls1}, function() {
-      // Notifying Console
-      chrome.extension.getBackgroundPage().console.log("Session 1 urls stored.");
-    });
-
+  chrome.storage.sync.set({['row'+row_num.toString()]: divRow}, function () {
+    chrome.extension.getBackgroundPage().console.log(divRow);
+    chrome.extension.getBackgroundPage().console.log("Row "+row_num.toString()+" saved to memory.");
   });
 
+  updateSessionsSaved();
+  saveSessionBtns = document.getElementsByClassName('saveSession');
+  openSessionBtns = document.getElementsByClassName('openSession');
 }
 
-// Defining Function for Clicking Open Button
-openSession1.onclick = function(element) {
+function updateSessionsSaved() {
+  saveSessionBtns = document.getElementsByClassName('saveSession');
+  openSessionBtns = document.getElementsByClassName('openSession');
 
-  // Retrieving the Session Urls from Storage
-  chrome.storage.sync.get(['sessionUrls1'], function(result) {
+  for (var i = 0; i < saveSessionBtns.length; i++) {
+    saveSessionBtns[i].onclick = function(element) {
 
-    // Notifying Console
-    chrome.extension.getBackgroundPage().console.log("Session 1 urls opened.");
+      var sessionSelected = element.toElement.id;
+      var sessionNumber = sessionSelected.substr(sessionSelected.length-1);
 
-    // Defining Variable to Refer to Session Urls Key
-    var sessionUrls1 = result.sessionUrls1;
+      // Defining Variable for Query Information on Tab Query
+      var queryInfo = {
+        currentWindow: true
+      }
 
-    // Looping through Tabs
-    for (var i = 0; i < sessionUrls1.length; i++) {
+      // Querying Tabs
+      chrome.tabs.query(queryInfo, function(tabs) {
 
-      // Getting Each Tab Url from the Session Array
-      var tabUrl = sessionUrls1[i];
+        // Defining Variable to Store Session Urls
+        var sessionUrls = [];
 
-      // Opening a New Tab with that Url
-      chrome.tabs.create({
-        url:tabUrl
+        // Looping Through Tabs
+        for (var i = 0; i < tabs.length; i++) {
+
+          // Storing the Tab Url in the Session Array
+          sessionUrls[i] = tabs[i].url;
+
+        };
+
+        //chrome.extension.getBackgroundPage().console.log(sessionUrls);
+
+        // Saving the Session Urls in Storage
+        chrome.storage.sync.set({['sessionUrls'+sessionNumber.toString()]: sessionUrls}, function() {
+          // Notifying Console
+          chrome.extension.getBackgroundPage().console.log("Session "+sessionNumber.toString()+" urls stored.");
+          chrome.extension.getBackgroundPage().console.log(sessionUrls);
+        });
+
       });
 
     }
 
-  });
+    openSessionBtns[i].onclick = function(element) {
+      var sessionSelected = element.toElement.id;
+      var sessionNumber = sessionSelected.substr(sessionSelected.length-1);
 
-}
+      // Retrieving the Session Urls from Storage
+      chrome.storage.sync.get(['sessionUrls'+sessionNumber.toString()], function(result) {
 
-// Defining References to Buttons on Dropdown Menu
-let saveSession2 = document.getElementById('saveSession2');
-let openSession2 = document.getElementById('openSession2');
+        chrome.extension.getBackgroundPage().console.log(result);
+        chrome.extension.getBackgroundPage().console.log(result.sessionUrls1);
 
-// Defining Function for Clicking Save Button
-saveSession2.onclick = function(element) {
+        // Notifying Console
+        chrome.extension.getBackgroundPage().console.log("Session "+sessionNumber.toString()+" urls opened.");
 
-  // Defining Variable for Query Information on Tab Query
-  var queryInfo = {
-    currentWindow: true
-  }
+        // Defining Variable to Refer to Session Urls Key
 
-  // Querying Tabs
-  chrome.tabs.query(queryInfo, function(tabs) {
+        var sessionUrls = result['sessionUrls'+sessionNumber.toString()];
 
-    // Defining Variable to Store Session Urls
-    var sessionUrls2 = [];
 
-    // Looping Through Tabs
-    for (var i = 0; i < tabs.length; i++) {
+        // Looping through Tabs
+        for (var i = 0; i < sessionUrls.length; i++) {
 
-      // Storing the Tab Url in the Session Array
-      sessionUrls2[i] = tabs[i].url;
+          // Getting Each Tab Url from the Session Array
+          var tabUrl = sessionUrls[i];
 
-    };
+          // Opening a New Tab with that Url
+          chrome.tabs.create({
+            url:tabUrl
+          });
 
-    //chrome.extension.getBackgroundPage().console.log(sessionUrls);
+        }
 
-    // Saving the Session Urls in Storage
-    chrome.storage.sync.set({'sessionUrls2': sessionUrls2}, function() {
-      // Notifying Console
-      chrome.extension.getBackgroundPage().console.log("Session 2 urls stored.");
-    });
-
-  });
-
-}
-
-// Defining Function for Clicking Open Button
-openSession2.onclick = function(element) {
-
-  // Retrieving the Session Urls from Storage
-  chrome.storage.sync.get(['sessionUrls2'], function(result) {
-
-    // Notifying Console
-    chrome.extension.getBackgroundPage().console.log("Session 2 urls opened.");
-
-    // Defining Variable to Refer to Session Urls Key
-    var sessionUrls2 = result.sessionUrls2;
-
-    // Looping through Tabs
-    for (var i = 0; i < sessionUrls2.length; i++) {
-
-      // Getting Each Tab Url from the Session Array
-      var tabUrl = sessionUrls2[i];
-
-      // Opening a New Tab with that Url
-      chrome.tabs.create({
-        url:tabUrl
       });
-
     }
-
-  });
-
+  }
 }
